79435abcde5d2528c250af0f1d20f7ebc68e4047
grahampugh <g.r.pugh@bristol.ac.uk>
2015-06-13 21:40:24 +0100

    Update enroll.php
    Allows up to four Included Manifests to be added during enrollment.

--- enroll.php
+++ enroll.php
@@ -2,51 +2,67 @@
 
 require_once( 'cfpropertylist-1.1.2/CFPropertyList.php' );
 
+// Default catalog
+$catalog = 'standard';
+
 // Get the varibles passed by the enroll script
-$identifier = $_POST["identifier"];
+$identifier1 = $_POST["identifier1"];
+$identifier2 = $_POST["identifier2"];
+$identifier3 = $_POST["identifier3"];
+$identifier4 = $_POST["identifier4"];
+
 $hostname   = $_POST["hostname"];
 
-// Split the manifest path up to determine directory structure
-$directories		= explode( "/", $identifier, -1 ); 
-$total				= count( $directories );
-$n					= 0;
-$identifier_path	= "";
-while ( $n < $total )
-    {
-        $identifier_path .= $directories[$n] . '/';
-        $n++;
-    }
-
-echo "\n\tMUNKI-ENROLLER. Checking for existing manifests.\n\n";
+// Ensure we aren't nesting a manifest within itself
+// Note that this will create a default manifest - it will not honour any options from DS
+if ( $identifier1 == "client-" . $hostname )
+	{
+		$identifier1 = "_cg_ru"; $identifier2 = "";
+	}
 
 // Check if manifest already exists for this machine
+echo "\n\tMUNKI-ENROLLER. Checking for existing manifests.\n\n";
+
 if ( file_exists( '../manifests/client-' . $hostname ) )
     {
-        echo "\tComputer manifest already exists.\n";
-        echo "\tPrevious settings will still apply to this computer.\n\n";
-        echo "\tYour manifest is: client-" . $hostname . "\n\n";
+        echo "\tComputer manifest client-" . $hostname . " already exists.\n";
+        echo "\tThis will be replaced.\n\n";
     }
 else
     {
         echo "\tComputer manifest does not exist. Will create.\n\n";
-        
-        $plist = new CFPropertyList();
-        $plist->add( $dict = new CFDictionary() );
-        
-        // Add manifest to production catalog by default
-        $dict->add( 'catalogs', $array = new CFArray() );
-        $array->add( new CFString( 'standard' ) );
-        
-        // Add parent manifest to included_manifests to achieve waterfall effect
-        $dict->add( 'included_manifests', $array = new CFArray() );
-        $array->add( new CFString( $identifier ) );
-        
-        // Save the newly created plist
-        $plist->saveXML( '../manifests/client-' . $hostname );
-        chmod( '../manifests/client-' . $hostname, 0777 );
-        echo "\tNew manifest created: client-" . $hostname . "\n";
-        echo "\tIncluded Manifest: " . $identifier . "\n";
-        
     }
+	$plist = new CFPropertyList();
+	$plist->add( $dict = new CFDictionary() );
+        
+    // Add manifest to production catalog by default
+    $dict->add( 'catalogs', $array = new CFArray() );
+    $array->add( new CFString( $catalog ) );
+        
+    // Add parent manifest to included_manifests to achieve waterfall effect
+    $dict->add( 'included_manifests', $array = new CFArray() );
+    if ( $identifier1 != "" )
+        {
+            $array->add( new CFString( $identifier1 ) );
+        }
+    if ( $identifier2 != "" )
+        {
+            $array->add( new CFString( $identifier2 ) );
+        }
+    if ( $identifier3 != "" )
+        {
+            $array->add( new CFString( $identifier3 ) );
+        }
+    if ( $identifier4 != "" )
+        {
+            $array->add( new CFString( $identifier4 ) );
+        }
 
+
+    // Save the newly created plist
+    $plist->saveXML( '../manifests/client-' . $hostname );
+    chmod( '../manifests/client-' . $hostname, 0775 );
+    echo "\tNew manifest created: client-" . $hostname . "\n";
+    echo "\tIncluded Manifest(s): " . $identifier1 . " " . $identifier2 . " " . $identifier3 . " " . $identifier4 . "\n";
+        
 ?>
